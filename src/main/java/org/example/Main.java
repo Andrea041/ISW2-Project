12444514ea2c6bcb5061b8367de87421d2f8b172
Andrea041 <116633306+Andrea041@users.noreply.github.com>
2024-04-09 22:38:54 +0200

    Implemented Release file info

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.example.controller.Metrics;
 import java.io.IOException;
 
 public class Main {
-    private static final String PROJECT_NAME = "BOOKKEEPER";
+    private static final String PROJECT_NAME = "OPENJPA";
 
     public static void main(String[] args) throws IOException {
         Metrics.dataExtraction(PROJECT_NAME);
