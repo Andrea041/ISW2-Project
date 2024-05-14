07924e066b5c4fb5c83b426e8f7b1b8b5ed7f9d5
Andrea041 <116633306+Andrea041@users.noreply.github.com>
2024-05-14 17:43:59 +0200

    remove smell

--- src/main/java/org/example/tool/FileARFFGenerator.java
+++ src/main/java/org/example/tool/FileARFFGenerator.java
@@ -8,18 +8,18 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileARFFGenerator {
-    private final String project_name;
+    private final String projectName;
 
-    public FileARFFGenerator(String project_name) {
-        this.project_name = project_name;
+    public FileARFFGenerator(String projectName) {
+        this.projectName = projectName;
     }
 
     public void csvToARFF() throws IOException {
         // Fetch .csv file
-        String csvFile = "/Users/andreaandreoli/Desktop/ISW2Falessi/MetricsML/" + this.project_name + "_trainingSet.csv";
+        String csvFile = "/Users/andreaandreoli/Desktop/ISW2Falessi/MetricsML/" + this.projectName + "_trainingSet.csv";
 
         // Where to save .arff file
-        String arffFile = "/Users/andreaandreoli/Desktop/ISW2Falessi/MetricsML/" + this.project_name + "_trainingSet.arff";
+        String arffFile = "/Users/andreaandreoli/Desktop/ISW2Falessi/MetricsML/" + this.projectName + "_trainingSet.arff";
 
         CSVLoader loader = new CSVLoader();
         loader.setSource(new File(csvFile));
